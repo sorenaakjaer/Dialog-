a107e8a90e624294ab962d6a92697cff7a86b89a
sorenaakjaer <77328754+sorenaakjaer@users.noreply.github.com>
2023-03-02 23:51:59 +0100

    Update load.js

--- load.js
+++ load.js
@@ -2,7 +2,7 @@ const params = new URLSearchParams(window.location.search)
 var version = params.get('VERSION')
 var random_id = new Date().valueOf();
 var external_path = 'https://cdn.jsdelivr.net/gh/sorenaakjaer/Dialog-@' + version + '/'
-
+var purge_path = 'https://cdn.jsdelivr.net/gh/sorenaakjaer/Dialog-/'
 var v_style1 = external_path + 'base-cbb.css' + '?' + random_id;
 var v_style2 = external_path + 'style.css' + '?' + random_id;
 var v_js1 = external_path + 'js-scripts.js'; //+ '?' + random_id;
@@ -10,23 +10,23 @@ var v_js2 = external_path + 'vue-scripts.js'; //+ '?' + random_id;
 var v_js3 = external_path + 'server.js'; //+ '?' + random_id;
 var v_html = external_path +'main.html' + '?' + random_id;
 
-Http.open("GET", external_path.replace('cdn.','purge.') + 'base-cbb.css');
+Http.open("GET", purge_path + 'base-cbb.css');
 Http.send();
 Http.onreadystatechange = (e) => {
   console.log(Http.responseText)
 }
-Http.open("GET", external_path.replace('cdn.','purge.') + 'style.css');
+Http.open("GET", purge_path + 'style.css');
 Http.send();
 Http.onreadystatechange = (e) => {
   console.log(Http.responseText)
 }
-Http.open("GET", external_path.replace('cdn.','purge.')+ 'js-scripts.js');
+Http.open("GET", purge_path + 'js-scripts.js');
 Http.send();
-Http.open("GET", external_path.replace('cdn.','purge.') + 'vue-scripts.js');
+Http.open("GET", purge_path + 'vue-scripts.js');
 Http.send();
-Http.open("GET", external_path.replace('cdn.','purge.') + 'server.js');
+Http.open("GET", purge_path + 'server.js');
 Http.send();
-Http.open("GET", external_path.replace('cdn.','purge.') + 'main.html');
+Http.open("GET", purge_path + 'main.html');
 Http.send();
 
   $('head').append('<link rel="stylesheet" href="'+ v_style1 +'" type="text/css" />');
